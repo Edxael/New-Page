97b53d232e94e587e4b79e2e2df05ac029fb627a
Edxael <zerphyr.r@gmail.com>
2017-03-21 10:56:40 -0600

    09-page V: 1.1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import ReacDOM from 'react-dom';
+import Comp_01 from './01-Comp.jsx';
+ 
+ 
+ReacDOM.render(<Comp_01 />, document.getElementById('root'));
